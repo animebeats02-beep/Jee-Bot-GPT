67d79323051e0d1ac8b38c4423460c8e535decb4
animebeats02-beep <animebeats02@gmail.com>
2026-06-13 04:53:50 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,10 @@
 import os
 
+from database import (
+    add_homework,
+    get_homework
+)
+
 from telegram import Update
 from telegram.ext import (
     Application,
@@ -18,6 +23,62 @@ async def start(
         "Hello Nihal! JEE Study OS is running."
     )
 
+async def addhw(
+    update: Update,
+    context: ContextTypes.DEFAULT_TYPE
+):
+
+    if len(context.args) < 2:
+
+        await update.message.reply_text(
+            "Usage:\n/addhw SUBJECT TASK"
+        )
+
+        return
+
+    subject = context.args[0]
+
+    task = " ".join(
+        context.args[1:]
+    )
+
+    add_homework(
+        subject,
+        task
+    )
+
+    await update.message.reply_text(
+        f"✅ Added:\n{subject} - {task}"
+    )
+    
+    async def tasks(
+    update: Update,
+    context: ContextTypes.DEFAULT_TYPE
+):
+
+    data = get_homework()
+
+    if len(data) == 0:
+
+        await update.message.reply_text(
+            "No homework."
+        )
+
+        return
+
+    text = "📚 Homework\n\n"
+
+    for row in data:
+
+        text += (
+            f"• {row[0]} - "
+            f"{row[1]}\n"
+        )
+
+    await update.message.reply_text(
+        text
+    )
+    
 def main():
 
     app = (
@@ -26,7 +87,19 @@ def main():
         .token(TOKEN)
         .build()
     )
+    app.add_handler(
+        CommandHandler(
+            "addhw",
+            addhw
+        )
+    )
 
+    app.add_handler(
+        CommandHandler(
+            "tasks",
+            tasks
+        )
+    )
     app.add_handler(
         CommandHandler(
             "start",
